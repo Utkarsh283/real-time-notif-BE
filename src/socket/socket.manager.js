a41c8a503709947ea211190af0634ec5de949c23
Your Personal Name <patelutkarsh283@gmail.com>
2025-05-16 11:34:31 +0530

    Add Notification

--- src/socket/socket.manager.js
+++ src/socket/socket.manager.js
@@ -1,5 +1,7 @@
 const { Server } = require('socket.io');
 const Notification = require('../models/apps/notifications/notifications.models.js');
+const Task = require('../models/apps/tasks/task.model.js');
+const User = require('../models/apps/auth/user.models.js');
 
 let io;
 
@@ -19,7 +21,7 @@ const setupSocket = (server) => {
   });
 
   // Watch MongoDB collection for inserts using Change Streams
- const changeStream = Notification.watch([], { fullDocument: 'updateLookup' });
+  const changeStream = Notification.watch([], { fullDocument: 'updateLookup' });
   changeStream.on('change', (change) => {
     if (change.operationType === 'insert') {
       const newNotification = change.fullDocument;
@@ -43,6 +45,60 @@ const setupSocket = (server) => {
       const deletedNotification = change.documentKey;
       io.emit('deleted-notification', deletedNotification); 
       console.log('Broadcasted deleted notification:', deletedNotification);
+    }
+  });
+
+  const taskChangeStream = Task.watch([], { fullDocument: 'updateLookup' });
+  taskChangeStream.on('change', (change) => {
+    if (change.operationType === 'insert') {
+      const newTask = change.fullDocument;
+      io.emit('new-task', newTask); 
+      console.log('Broadcasted new task:', newTask);
+    }
+
+    if (change.operationType === 'update') {
+      const updatedTask = change.fullDocument;
+      io.emit('updated-task', updatedTask); 
+      console.log('Broadcasted updated task:', updatedTask);
+    }
+
+    if (change.operationType === 'replace') {
+      const replacedTask = change.fullDocument;
+      io.emit('replaced-task', replacedTask); 
+      console.log('Broadcasted replaced task:', replacedTask);
+    }
+
+    if (change.operationType === 'delete') {
+      const deletedTask = change.documentKey;
+      io.emit('deleted-task', deletedTask); 
+      console.log('Broadcasted deleted task:', deletedTask);
+    }
+  });
+
+  const userChangeStream = User.watch([], { fullDocument: 'updateLookup' });
+  userChangeStream.on('change', (change) => {
+    if (change.operationType === 'insert') {
+      const newUser = change.fullDocument;
+      io.emit('new-user', newUser); 
+      console.log('Broadcasted new user:', newUser);
+    }
+
+    if (change.operationType === 'update') {
+      const updatedUser = change.fullDocument;
+      io.emit('updated-user', updatedUser); 
+      console.log('Broadcasted updated user:', updatedUser);
+    }
+
+    if (change.operationType === 'replace') {
+      const replacedUser = change.fullDocument;
+      io.emit('replaced-user', replacedUser); 
+      console.log('Broadcasted replaced user:', replacedUser);
+    }
+
+    if (change.operationType === 'delete') {
+      const deletedUser = change.documentKey;
+      io.emit('deleted-user', deletedUser); 
+      console.log('Broadcasted deleted user:', deletedUser);
     }
   });
 };
